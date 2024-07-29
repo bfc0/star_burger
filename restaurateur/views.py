3c1163eba4963a19e8805d1d0572f7baa081ea1f
bfc0 <bfcheg@gmail.com>
2024-07-29 17:30:44 +0300

    Removed crashing view

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -131,7 +131,3 @@ def view_orders(request):
     return render(request, template_name='order_items.html', context={
         "order_items": orders
     })
-
-
-def crashing_view(request):
-    raise
